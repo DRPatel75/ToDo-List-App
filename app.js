5aa7c64fca9f3fc8e2a2e066cdef02ebb4f235ec
DRPatel75 <drpatel3009@gmail.com>
2026-05-06 21:02:26 +0530

    Saving todos to localstorage

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 const addTodoBtn = document.getElementById("addTodoBtn")
 const inputTag = document.getElementById("todoInput")
 let todoText; //This should be populated when the user clicks on Add button
+//If we have todos in localStorage, we will read it 
 let todos = [];
+let todoString = localStorage.getItem("todos")
+if(todoString){
+    todos = JSON.parse(todoString)
+}
 
 addTodoBtn.addEventListener("click", ()=>{
     console.log("Hey I just Clicked")
     todoText = inputTag.value
     console.log(todoText)
     inputTag.value = ""
+    let todo = {
+        title: todoText,
+        iscompleted: false
+    }
+    todos.push(todo)
+    localStorage.setItem("todo", JSON.stringify(todos))
 })
